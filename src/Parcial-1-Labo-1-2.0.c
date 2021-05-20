276b5286074306b1ae87515bb2a8d15e63b78a45
ornecurcio <ornecurcio@gmail.com>
2021-05-20 18:43:49 -0300

    fist commit

--- src/Parcial-1-Labo-1-2.0.c
+++ src/Parcial-1-Labo-1-2.0.c
@@ -1,6 +1,6 @@
 /*
  ============================================================================
- Name        : 0.c
+ Name        : Ornela Curcio.c
  Author      : 
  Version     :
  Copyright   : Your copyright notice
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Hello Orne!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
